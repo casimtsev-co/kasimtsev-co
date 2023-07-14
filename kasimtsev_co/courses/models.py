46f9016f9ebe9b2795c1d3cfb801817622635b0e
Patrick Su <patrickthesu@gmail.com>
2023-07-14 11:16:43 +0300

    removed old lib not req

--- kasimtsev_co/courses/models.py
+++ kasimtsev_co/courses/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import RegexValidator
 
 #from .users.models import Teacher, Student
--- kasimtsev_co/kasimtsev_co/settings.py
+++ kasimtsev_co/kasimtsev_co/settings.py
@@ -35,8 +35,6 @@ INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
     'users.apps.UsersConfig',
 
-    "phonenumber_field",
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
